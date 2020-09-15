3ac77d37b2c30b09097511e90788804c52ac8043
小飞侠 <2327220169@qq.com>
2020-09-15 16:49:42 +0800

    跨域

--- app/Http/Middleware/CheckAgent.php
+++ app/Http/Middleware/CheckAgent.php
@@ -19,13 +19,30 @@ class CheckAgent
     {
         $chatAuthService = new ChatAuthService($request);
         //禁止未知设备访问
-        if($chatAuthService->forbiddenRequest()) return response()->json(['code'=>10010,'msg'=>'禁止未知设备访问！'],401);
+        //if($chatAuthService->forbiddenRequest()) return response()->json(['code'=>10010,'msg'=>'禁止未知设备访问！'],401);
         //禁止游客访问
         if($touristMode){
             if($chatAuthService->isTourist()) return response()->json(['code'=>1011,'msg'=>'禁止游客访问！'],401);
         }
 
-        return $next($request);
+        //多域名-CORS跨域方案-服务端
+        $response = $next($request);
+        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
+        $allowOrigin = array(
+            'http://ai.mycjj.com:8091','http://web.newweb.com', 'http://web.chejj.cn', 'http://web.mycjj.com', 'https://web.chejj.cn', 'https://web.mycjj.com',
+            'http://admin.newweb.com', 'http://admin.chejj.cn', 'http://admin.mycjj.com', 'https://admin.chejj.cn', 'https://admin.mycjj.com',
+            'http://www.newweb.com', 'http://www.chejj.cn', 'http://www.mycjj.com', 'https://www.chejj.cn', 'https://www.mycjj.com',
+            'http://4s.newweb.com', 'http://4s.chejj.cn', 'http://4s.mycjj.com', 'https://4s.chejj.cn', 'https://4s.mycjj.com',
+            'http://api.newweb.com', 'http://api.chejj.cn', 'http://api.mycjj.com', 'https://api.chejj.cn', 'https://api.mycjj.com');
+        $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+        if (in_array($origin, $allowOrigin)) {
+            $response->header('Access-Control-Allow-Origin:' . $origin);
+        }
+        $response->header('Access-Control-Allow-Headers:Origin,X-Requested-With,Content-Type,Accept,Authorization,Cookie');
+        $response->header('Access-Control-Allow-Methods:GET,POST');
+        $response->header("Access-Control-Allow-Credentials:true"); //允许携带cookie
+        $response->header("Access-Control-Max-Age:1728000"); //减少预检请求次数
 
+        return $next($request);
     }
 }
