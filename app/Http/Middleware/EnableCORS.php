28f39945a198ab1bcbaa5b01742d8609d02afde5
小飞侠 <2327220169@qq.com>
2020-09-16 11:21:37 +0800

    跨域中间件

---
+++ app/Http/Middleware/EnableCORS.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class EnableCORS
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //多域名-CORS跨域方案-服务端
+        $response = $next($request);
+        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
+        $allowOrigin = array(
+            'http://ai.mycjj.com:8091','http://web.newweb.com', 'http://web.chejj.cn', 'http://web.mycjj.com', 'https://web.chejj.cn', 'https://web.mycjj.com',
+            'http://admin.newweb.com', 'http://admin.chejj.cn', 'http://admin.mycjj.com', 'https://admin.chejj.cn', 'https://admin.mycjj.com',
+            'http://www.newweb.com', 'http://www.chejj.cn', 'http://www.mycjj.com', 'https://www.chejj.cn', 'https://www.mycjj.com',
+            'http://4s.newweb.com', 'http://4s.chejj.cn', 'http://4s.mycjj.com', 'https://4s.chejj.cn', 'https://4s.mycjj.com',
+            'http://api.newweb.com', 'http://api.chejj.cn', 'http://api.mycjj.com', 'https://api.chejj.cn', 'https://api.mycjj.com');
+        if (in_array($origin, $allowOrigin)) {
+            $response->header('Access-Control-Allow-Origin',$origin);
+        }
+        $response->header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization,Cookie');
+        $response->header('Access-Control-Allow-Methods','GET,POST,PUT');
+        $response->header('Access-Control-Allow-Credentials','true'); //允许携带cookie
+        $response->header('Access-Control-Max-Age','1728000'); //减少预检请求次数
+
+        return $response;
+    }
+}
